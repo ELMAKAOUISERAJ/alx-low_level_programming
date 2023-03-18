347ba3e2ace0cdc21dabefc823afeff2c32753bb
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-17 18:01:53 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main(void)
-	/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+/**
+ * main--entry point
+ * returne lawys 0
  */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 		return (0);

600a5edcdaaa84e7bd0f5a5b8df13cd4a44aabbf
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-20 16:57:55 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description
+ * return: always 0 succes
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	putchar('\n');
+}

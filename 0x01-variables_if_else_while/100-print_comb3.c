34eda8efde963026812bf292fc65b4c65f47616c
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-20 19:57:45 -0700

    9

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,14 @@ int main(void)
 {
 	int x;
 	int y;
+
 	for (x = 0; x <= 9; x++)
 	{
 		for (y = x + 1; y <= 9; y++)
 		{
 			putchar(x + '0');
 			putchar(y + '0');
-			if ( x < 8)
+			if( x < 8)
 			{
 				putchar(',');
 				putchar(' ');

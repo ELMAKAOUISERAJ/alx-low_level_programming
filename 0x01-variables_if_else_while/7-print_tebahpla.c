82f986a120bc019d7fcc61fe0940c9ef077010eb
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-20 18:13:31 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabet in reverse
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x <= 'a'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}

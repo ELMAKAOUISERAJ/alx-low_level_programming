3ffe19a2bb47458d878cae498c4932a653e90f53
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-20 18:02:39 -0700

    5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - all single digit numbers of base 10 starting from 0
+ *
+ * Return: always 0 succes
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}

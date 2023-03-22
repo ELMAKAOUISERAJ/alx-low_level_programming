3c6da20e683e8acd7d5eeed070e107b62b563371
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-21 18:28:45 -0700

    #1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main -ETRY
+ *
+ * Return: always à success
+ */
+void print_alphabet(void);
+{
+	int i;
+	for (i = 'a'; i <= 'z')
+		putchar(i);
+	putchar('\n');
+	return (0);
+}

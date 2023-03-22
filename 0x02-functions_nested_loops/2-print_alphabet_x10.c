563b972b99ad8cbb991c731b277b067885872f82
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-21 18:51:10 -0700

    #1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -Etry point
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int x, y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		{
+			_putchar(x);
+		}
+		putchar('\n');
+	}
+}

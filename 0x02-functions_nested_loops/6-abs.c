f9c8b2896a2e703f081ef95592e2c0ce35ec3160
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-22 17:24:13 -0700

    pr

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - give the absolut value
+ * @n: input number
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0);
+	{
+		return (n);
+	}
+	else
+	{
+		rerurn (n * -1);
+	}
+}

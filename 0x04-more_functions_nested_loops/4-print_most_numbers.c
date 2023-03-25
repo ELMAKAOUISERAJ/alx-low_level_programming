aeeac8679b164bc63cbbf77b72de4b1fc2df2c1e
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-24 18:40:47 -0700

    1

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,11 +3,12 @@
 /**
  * print_most_numbers -t prints the numbers, from 0 to 9 except 2 and 4
  */
+
 void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 		if (c != '2' && c != '4')
 			_putchar(i);
 	_putchar('\n');

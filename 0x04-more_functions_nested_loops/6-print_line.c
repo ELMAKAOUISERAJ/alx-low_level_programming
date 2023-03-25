92daffa28a8f2cf507299563b3982289c29731d1
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-03-24 19:13:52 -0700

    1

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,16 @@
 */
 void print_line(int n)
 {
-	char c;
+	int c;
 
-	for (c = 0; c < n; c++)
-		if (n == 0)
+		if (n <= 0)
 		{
 			_putchar('\n');
 		}
 		else
+			for (c = 0; c <= n; c++)
 		{
-		_putchar('_');
+		_putchar(95);
 	_putchar('\n');
 		}
 }

7f91b0b48f333e6d8a7337d1dff246db4772557c
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-04 17:19:11 -0700

    &

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: distination
+ * @src: source
+ * @n: lenght of buffer
+ * Return: the new string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	while (i < n)
+	{
+		*(dest + i) = *(src + i)
+			i++;
+	}
+	return (dest);
+}

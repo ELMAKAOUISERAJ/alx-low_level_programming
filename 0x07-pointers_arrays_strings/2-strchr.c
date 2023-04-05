8efd86b08e5d9d002d3c0b7c9cbae4807134dd22
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-04 17:39:16 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locate a character
+ * @s: source string
+ * @c: wanted char
+ * Return:the new  string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		else
+			s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+		else
+			return (0);
+	}
+}

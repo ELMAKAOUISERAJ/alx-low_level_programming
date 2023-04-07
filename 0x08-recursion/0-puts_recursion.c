b03dd01cd463666d573cd43782c1f599fc09e798
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-06 18:47:35 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string followed by new line
+ *@s: source string
+ *Return: 1 on succes
+ */
+void _puts_recursion(char *s)
+{
+if (*s == 0)
+{
+	_putchar('\n');
+}
+_putchar(*s);
+_puts_recursion(*s + 1);
+}

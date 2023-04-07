b9d3fca3354bc9cffb239bbdcb21e73f039ec117
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-06 19:36:17 -0700

    3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * factorial - print factorial for given number
+ * @n: given number
+ * Retuen : the factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (factorial(n - 1) * n);
+}
+

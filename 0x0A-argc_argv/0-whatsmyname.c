5ba6ce342c885375c401f78dd1c6c81398757501
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-15 10:07:05 -0700

    task 0 argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print my name
+ * @argc: integer
+ * @argv: array
+ * Return: 0 or nULL
+ */
+int main(int argc, char const argv[])
+{
+void(argc);
+printf("%s\n", argv[0]);
+return (0);
+}

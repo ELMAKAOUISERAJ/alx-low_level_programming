c276ec22493b332d48b0f5a379e2ff65d29260b6
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-15 16:53:32 -0700

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints its name followed by new line
- * @argc: n of parameters 
+ * @argc: nb arguments
  * @argv: array of parameters
  * Return: alwaus 0 succes
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);

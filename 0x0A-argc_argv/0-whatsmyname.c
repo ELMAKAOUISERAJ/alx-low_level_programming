5ff42703f69709822c83be0b1478237978fc81bc
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-15 10:19:25 -0700

    task 0 argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints its name followed by new line
+ * @argc: n of parameters 
+ * @argv: array of parameters
+ * Return: alwaus 0 succes
+ */
+int main(int argc, char const argv[])
+{
+	printf("%s\d", argv[0]);
+	return (0);
+}

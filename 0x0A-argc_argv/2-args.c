5fee07e4d8aaa9e3ec445de91b4bed52224b0f44
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-15 15:17:22 -0700

    TASK 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all argument it receive
+ * @argc: nb of argument
+ * @argv: array of argument
+ * Return: always 0 succes
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0, i < argc; i++)
+		printf("%d\n", argv[i]);
+}
+return (0);
+}

d10944c24c6ea0ec8dbaa00af4f41894f1ddfb99
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-15 10:43:55 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
  * main - print number of argument passed
  * @argc: num of parameters
  * @argv: array of parameters
- * REturn: 0
+ * Return: Alaws  0 succes
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {

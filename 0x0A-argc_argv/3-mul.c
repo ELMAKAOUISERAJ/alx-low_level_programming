31db72df0fdabdcbf149700b59ffb57ac82757b5
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-15 16:09:40 -0700

    4TASK

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - multiple two numbers
+ * @argc: nb of arguments
+ * @argv: array of arrguments
+ * Return: always 0 succes
+ */
+int main(int argc, char *argv[])
+{
+	int mult, a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2];
+			mult = a * b;
+			printf("%d\n", mult);
+			return (0);
+			}

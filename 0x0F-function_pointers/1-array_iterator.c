fbda95e3ad2e10cd53e902d6cc082fc2a0833d2f
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-04-25 06:47:38 -0700

    0

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers"
+/**
+ * array_eterator - execute a function
+ * @array: input array
+ * @size: size of array
+ * @action: input function
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{ 
+	int i;
+
+	if (action && array)
+	{
+		for(i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}

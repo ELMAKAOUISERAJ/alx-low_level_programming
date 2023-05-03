9390b393efec3c5eca89c751374f49b69595e9d0
ELMAKAOUISERAJ <elmakawiabdelghani@gmail.com>
2023-05-03 16:15:49 -0700

    TA1 compil

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,12 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdlib.h>
 /**
- * list_len - finds the number of nodes in a list
+ * listint_len - returns the number of elements in a linked listint_t list
  * @h: singly linked list
- * Return: number of elements in a linked list
+ * Return: number of nodes
  */
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
-	int i;
+	size_t i;
 
 	for (i = 0; h; i++)
 		h = h->next;
